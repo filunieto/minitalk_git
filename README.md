72f36bde865635c5073711e3bf9a7205dd3800ab
filunie <filunie@gmail.com>
2022-08-20 13:59:57 +0200

    1st Step: make server receive a signal from client~2

--- README.md
+++ README.md
@@ -6,11 +6,11 @@ minitalk proyect 42
 
 Small data exchange program using UNIX signals.
  
- [![Intro](https://img.shields.io/badge/Cursus-Minitalk-success?style=for-the-badge&logo=42)](https://github.com/mlanca-c/Minitalk)
+ [![Intro](https://img.shields.io/badge/Cursus-Minitalk-success?style=for-the-badge&logo=42)](https://github.com/filunieto/minitalk_git)
  
- [![Stars](https://img.shields.io/github/stars/mlanca-c/Minitalk?color=ffff00&label=Stars&logo=Stars&style=?style=flat)](https://github.com/mlanca-c/Minitalk)
- [![Size](https://img.shields.io/github/repo-size/mlanca-c/Minitalk?color=blue&label=Size&logo=Size&style=?style=flat)](https://github.com/mlanca-c/Minitalk)
- [![Activity](https://img.shields.io/github/last-commit/mlanca-c/Minitalk?color=red&label=Last%20Commit&style=flat)](https://github.com/mlanca-c/Minitalk)
+ [![Stars](https://img.shields.io/github/stars/filunieto/minitalk_git?color=ffff00&label=Stars&logo=Stars&style=?style=flat)](https://github.com/filunieto/minitalk_git)
+ [![Size](https://img.shields.io/github/repo-size/filunieto/minitalk_git?color=blue&label=Size&logo=Size&style=?style=flat)](https://github.com/filunieto/minitalk_git)
+ [![Activity](https://img.shields.io/github/last-commit/filunieto/minitalk_git?color=red&label=Last%20Commit&style=flat)](https://github.com/filunieto/minitalk_git)
 
 # Cloning
 
--- src/client.c
+++ src/client.c
@@ -1,31 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   server.c                                           :+:      :+:    :+:   */
+/*   client.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fnieves <fnieves@42heilbronn.de>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/08/20 07:03:13 by fnieves           #+#    #+#             */
-/*   Updated: 2022/08/20 11:26:59 by fnieves          ###   ########.fr       */
+/*   Created: 2022/08/20 12:07:05 by fnieves           #+#    #+#             */
+/*   Updated: 2022/08/20 12:12:00 by fnieves          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/mini_talk.h"
 
-void	handler_sigusr1(int signum)
+int	main(int argc, char **argv)
 {
-	printf("signal %d received.\n", signum);
-}
-
-int main(void)
-{
-	pid_t	pid;
-
-	pid = getpid();
-	printf("PID: %d\n", pid);
-	signal(SIGUSR1, handler_sigusr1);
-	while (1)
-		pause();
-	return(0);
+	int	pid;
 	
+	if (argc != 2)
+	{
+		printf("client: invalid number of arguments. 2 argumentos máximo\n");
+		exit(EXIT_FAILURE);
+	}
+	pid = atoi(argv[1]);
+	kill(pid, SIGUSR1);
+	return (0);
 }
